f5dd08bfe4f1a5f76d6f422f79d54dbd3700c4d8
Marcella Chaves <marcellafccosta@gmail.com>
2024-09-26 11:21:36 -0300

    crud endereco e crud usuario com endereco

--- src/main/java/com/sisAluguel/sisAluguel/Model/Usuario.java
+++ src/main/java/com/sisAluguel/sisAluguel/Model/Usuario.java
@@ -1,10 +1,13 @@
 package com.sisAluguel.sisAluguel.Model;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 
@@ -35,6 +38,11 @@ public class Usuario {
     private Double rendimentoAuferido;
 
     private String tipo;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "endereco_id", referencedColumnName = "idEndereco")
+    private Endereco endereco;
+
 
     // Getters e Setters
 
@@ -101,4 +109,47 @@ public class Usuario {
     public void setTipo(String tipo) {
         this.tipo = tipo;
     }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+    
 }
+
+
+/*
+ * CREATE TABLE IF NOT EXISTS endereco(
+	id INT AUTO_INCREMENT PRIMARY KEY, 
+	logradouro VARCHAR(100) NOT NULL,
+    bairro VARCHAR(50) NOT NULL, 
+    cidade VARCHAR(50) NOT NULL, 
+    estado VARCHAR(50) NOT NULL, 
+    cep VARCHAR(8) NOT NULL, 
+    numero INT NOT NULL, 
+    complemento VARCHAR(20) 
+);
+ */
+
+ /*
+  * 
+
+  CREATE TABLE IF NOT EXISTS usuario (
+    id INT AUTO_INCREMENT PRIMARY KEY,  
+    nome VARCHAR(255) NOT NULL,          
+    cpf VARCHAR(14) NOT NULL,            
+    rg VARCHAR(20), 
+    tipo ENUM ('cliente', 'agente') NOT NULL,
+    profissao VARCHAR(100),              
+    entidade_empregadora VARCHAR(255),
+    rendimento_auferido DOUBLE, 
+    endereco_id INT,   -- Chave estrangeira para a tabela endereco
+    FOREIGN KEY (endereco_id) REFERENCES endereco(id) ON DELETE CASCADE
+);
+
+
+ */
--- src/main/java/com/sisAluguel/sisAluguel/Service/UsuarioService.java
+++ src/main/java/com/sisAluguel/sisAluguel/Service/UsuarioService.java
@@ -3,7 +3,9 @@ package com.sisAluguel.sisAluguel.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.sisAluguel.sisAluguel.Model.Endereco;
 import com.sisAluguel.sisAluguel.Model.Usuario;
+import com.sisAluguel.sisAluguel.Repository.EnderecoRepository;
 import com.sisAluguel.sisAluguel.Repository.UsuarioRepository;
 
 import java.util.List;
@@ -13,6 +15,9 @@ public class UsuarioService {
     @Autowired
     private UsuarioRepository usuarioRepository;
 
+    @Autowired
+    private EnderecoRepository enderecoRepository;  // Injeta o repositório de endereço
+
     public List<Usuario> findAll() {
         return usuarioRepository.findAll();
     }
@@ -21,7 +26,11 @@ public class UsuarioService {
         return usuarioRepository.findById(id).orElse(null);
     }
 
-    public Usuario save(Usuario usuario) {
+     public Usuario save(Usuario usuario) {
+        if (usuario.getEndereco() != null) {
+            Endereco savedEndereco = enderecoRepository.save(usuario.getEndereco());
+            usuario.setEndereco(savedEndereco);  
+        }
         return usuarioRepository.save(usuario);
     }
 
