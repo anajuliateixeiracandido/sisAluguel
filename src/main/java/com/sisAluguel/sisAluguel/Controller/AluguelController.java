a437d1a5f17926b590ddd860ff385b72bba44119
Ana Cândido <128816273+anajuliateixeiracandido@users.noreply.github.com>
2024-09-28 19:01:46 -0300

    front

--- src/main/java/com/sisAluguel/sisAluguel/Controller/AluguelController.java
+++ src/main/java/com/sisAluguel/sisAluguel/Controller/AluguelController.java
@@ -3,53 +3,88 @@ package com.sisAluguel.sisAluguel.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.sisAluguel.sisAluguel.Model.Aluguel;
 import com.sisAluguel.sisAluguel.Service.AluguelService;
 
-
-@RestController
+@Controller
 @RequestMapping("/aluguel")
 public class AluguelController {
+
     @Autowired
     private AluguelService aluguelService;
 
-    @GetMapping
-    public List<Aluguel> getAll() {
-        return aluguelService.findAll();
-    }
+    @GetMapping("/")
+    public ModelAndView getAll() {
+        List<Aluguel> alugueis = aluguelService.findAll();
+        ModelAndView mv = new ModelAndView("aluguel_list");
+        mv.addObject("alugueis", alugueis);  // Mudei "alugueis" para "aluguels"
+        return mv;
+    }    
 
+
+    // Exibe os detalhes de um aluguel específico
     @GetMapping("/{idAluguel}")
-    public ResponseEntity<Aluguel> getById(@PathVariable Long idAluguel) {
+    public ModelAndView getById(@PathVariable Long idAluguel) {
         Aluguel aluguel = aluguelService.findById(idAluguel);
-        return aluguel != null ? ResponseEntity.ok(aluguel) : ResponseEntity.notFound().build();
+        ModelAndView mv = new ModelAndView("aluguel_detalhes");
+
+        if (aluguel != null) {
+            mv.addObject("aluguel", aluguel);
+        } else {
+            mv.setViewName("not_found");
+        }
+        return mv;
     }
 
-    @PostMapping
-    public Aluguel create(@RequestBody Aluguel aluguel) {
-        return aluguelService.save(aluguel);
+    // Exibe a tela de formulário para criar novo aluguel
+    @GetMapping("/novo")
+    public ModelAndView createForm() {
+        ModelAndView mv = new ModelAndView("aluguel_form");
+        mv.addObject("aluguel", new Aluguel());
+        return mv;
     }
 
-    @PutMapping("/{idAluguel}") 
-    public ResponseEntity<Aluguel> update(@PathVariable Long idAluguel, @RequestBody Aluguel aluguel) {
-        aluguel.setIdAluguel(idAluguel);
-        Aluguel updatedAluguel = aluguelService.save(aluguel);
-        return ResponseEntity.ok(updatedAluguel);
-    }
-
-    @DeleteMapping("/{idAluguel}")
-    public ResponseEntity<Void> delete(@PathVariable Long idAluguel) {
-    aluguelService.delete(idAluguel);
-    return ResponseEntity.noContent().build();
-    }
     
+
+    // Salva um novo aluguel
+    @PostMapping("/novo")
+    public ModelAndView create(Aluguel aluguel) {
+        aluguelService.save(aluguel);
+        return new ModelAndView("redirect:/aluguel/");
+    }
+
+    // Exibe a tela de formulário para editar um aluguel existente
+    @GetMapping("/editar/{idAluguel}")
+    public ModelAndView editForm(@PathVariable Long idAluguel) {
+        Aluguel aluguel = aluguelService.findById(idAluguel);
+        ModelAndView mv = new ModelAndView("aluguel_form");
+
+        if (aluguel != null) {
+            mv.addObject("aluguel", aluguel);
+        } else {
+            mv.setViewName("not_found");
+        }
+        return mv;
+    }
+
+    // Atualiza um aluguel existente
+    @PostMapping("/editar")
+    public ModelAndView update(Aluguel aluguel) {
+        aluguelService.save(aluguel);
+        return new ModelAndView("redirect:/aluguel/");
+    }
+
+    // Deleta um aluguel
+    @GetMapping("/deletar/{idAluguel}")
+    public ModelAndView delete(@PathVariable Long idAluguel) {
+        aluguelService.delete(idAluguel);
+        return new ModelAndView("redirect:/aluguel/");
+    }
 }
